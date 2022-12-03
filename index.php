04ea2f71c36ebd11124256295defd9448856f6d3
Evergyu <iingy123@naver.com>
2022-12-03 21:34:10 +0900

    Set apache

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+phpinfo();
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Leeingyu</title>
+    </head>
+    <body>
+        <h1>Web</h1>
+        <ol>
+            <li>HTML</li>
+        </ol>
+    </body>
+</html>
+
